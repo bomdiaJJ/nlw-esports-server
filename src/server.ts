3e561f82a21887a81ec61e5cd57491ec0737fa62
JJ <joaoart42@gmail.com>
2022-09-14 01:40:21 -0300

    :card_file_box: cria um banco de dados

--- src/server.ts
+++ src/server.ts
@@ -1,23 +1,28 @@
-import express from 'express'
+import express, { response } from 'express'
 
 const app = express()
 
+app.get('/games', (request, response) => {
+    return response.json([])
+})
 
-// www.minhaapi.com/ads
-app.get('/ads', (request, response) => {
-    // console.log(request) - doideira
-    // console.log('Acessou Ads!')
+app.post('/ads', (request, response) => {
+    return response.status(201).json([])
+})
 
-    // return response.send('Acessou Ads!') responde pro usuário
-    
-    return response.json([ //responde um json pro usuário
-        { id: 1, name: 'Anuncio 1' },
-        { id: 2, name: 'Anuncio 2' },
-        { id: 3, name: 'Anuncio 3' },
-        { id: 4, name: 'Anuncio 4' },
-        { id: 5, name: 'Anuncio 5' },
+app.get('/ads/:id/discord', (request, response) => {
+    const adId = request.params.id;
+
+    return response.json([
     ]) 
 })
 
-// localhost:3333/ads
+app.get('/games/:id/ads', (request, response) => {
+    const gameId = request.params.id;
+    
+    return response.json([
+    ]) 
+})
+
+// localhost:3333/
 app.listen(3333)
